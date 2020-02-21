bfba809fb7d5b3b1ea578520b1630047d90d99ef
Adrian <e@essenciary.com>
2020-02-21 07:20:44 +0100

    connection refactoring

--- src/SearchLightPostgreSQL.jl
+++ src/SearchLightPostgreSQL.jl
@@ -72,8 +72,15 @@ end
 
 Disconnects from database.
 """
-function SearchLight.disconnect(conn::DatabaseHandle = CONNECTIONS[end]) :: Nothing
+function SearchLight.disconnect(conn::DatabaseHandle = SearchLight.connection()) :: Nothing
   LibPQ.close(conn)
+end
+
+
+function SearchLight.connection()
+  isempty(CONNECTIONS) && throw(SearchLight.Exceptions.NotConnectedException())
+
+  CONNECTIONS[end]
 end
 
 
@@ -121,7 +128,7 @@ Escapes the column name.
 julia>
 ```
 """
-function SearchLight.escape_column_name(c::String, conn::DatabaseHandle = CONNECTIONS[end]) :: String
+function SearchLight.escape_column_name(c::String, conn::DatabaseHandle = SearchLight.connection()) :: String
   join(["""\"$(replace(cx, "\""=>"'"))\"""" for cx in split(c, '.')], '.')
 end
 
@@ -136,7 +143,7 @@ Escapes the value `v` using native features provided by the database backend if 
 julia>
 ```
 """
-function SearchLight.escape_value(v::T, conn::DatabaseHandle = CONNECTIONS[end])::T where {T}
+function SearchLight.escape_value(v::T, conn::DatabaseHandle = SearchLight.connection())::T where {T}
   isa(v, Number) ? v : "E'$(replace(string(v), "'"=>"\\'"))'"
 end
 
@@ -146,7 +153,7 @@ end
 #
 
 
-function SearchLight.query(sql::String, conn::DatabaseHandle = CONNECTIONS[end]; internal = false) :: DataFrames.DataFrame
+function SearchLight.query(sql::String, conn::DatabaseHandle = SearchLight.connection(); internal = false) :: DataFrames.DataFrame
   result = if SearchLight.config.log_queries && ! internal
     @info sql
     @time LibPQ.execute(conn, sql)
@@ -245,7 +252,7 @@ end
 
 
 function SearchLight.to_from_part(m::Type{T})::String where {T<:SearchLight.AbstractModel}
-  "FROM " * SearchLight.escape_column_name(SearchLight.table_name(m()), CONNECTIONS[end])
+  "FROM " * SearchLight.escape_column_name(SearchLight.table_name(m()), SearchLight.connection())
 end
 
 
