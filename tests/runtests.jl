5091c33a610e2cf29745f3490c5915c38bd7e204
Frank Urbach <frank.urbach@gmx.de>
2021-12-14 01:04:35 +0100

    next round tests

--- tests/runtests.jl
+++ tests/runtests.jl
@@ -3,6 +3,14 @@ using Pkg
 using Test, TestSetExtensions, SafeTestsets
 using SearchLight
 
-@testset ExtendedTestSet "SearchLight PostgreSQL adapter tests" begin
-  @includetests ARGS
-end
+# @testset ExtendedTestSet "SearchLight PostgreSQL adapter tests" begin
+#   @includetests ARGS
+# end
+
+# run a simple connect test for the first time
+
+connection_file = joinpath(@__DIR__,"postgres_connection.yml")
+conn_info_postgres = SearchLight.Configuration.load(connection_file)
+conn = SearchLight.connect(conn_info_postgres)
+
+SearchLight.disconnect(conn)
